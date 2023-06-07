5a8716b7344afa03f342e16ca29adeabc5c3ce1e
zachary-shah <123213526+zachary-shah@users.noreply.github.com>
2023-06-07 01:29:13 -0700

    uploaded figures!

--- cifar10_condition_numbers.py
+++ cifar10_condition_numbers.py
@@ -7,24 +7,17 @@ Script generates:
 
 import numpy as np
 import matplotlib
-#matplotlib.use('WebAgg')
+matplotlib.use('WebAgg')
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 import argparse
-import time
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
-
-from relu_solver import CReLU_MLP
-from utils.relu_utils import classifcation_accuracy, binary_classifcation_accuracy, squared_loss, cross_entropy_loss
-from utils.load_data import load_mnist, load_cifar
+from utils.load_data import load_cifar
 from datetime import datetime
 import os
 dtime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-from utils.admm_utils import ADMM_Params, FG_Operators, get_hyperplane_cuts
+from utils.admm_utils import FG_Operators, get_hyperplane_cuts, nystrom_sketch
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -45,6 +38,7 @@ def parse_args():
     return args
 
 args = parse_args()
+P_S = args.P_S
 
 # ------------ Load Data ------------
 print(f'Loading data...')
@@ -53,12 +47,10 @@ os.makedirs(args.save_root, exist_ok=True)
 X_train, y_train, _, _ = load_cifar(dataset_rel_path=args.dataset_path, n=args.n_train, downsample=args.downsample)
 X = np.hstack([X_train, np.ones((X_train.shape[0],1))])
 
-
-
-P_S = 10
+print("Computing A...")
 
 # construct d-diags
-d_diags = get_hyperplane_cuts(X, 10, seed=1)
+d_diags = get_hyperplane_cuts(X, P_S, seed=1)
 OPS = FG_Operators(d_diags, X, rho=0.001)
 n, d = X.shape
 # construct A
@@ -67,8 +59,30 @@ for i in range(P_S):
     for j in range(P_S):
         # perform multiplication 
         FiFj = OPS.F(i % P_S).T @ OPS.F(j % P_S) / OPS.rho
-
         A[i*d:(i+1)*d, j*d:(j+1)*d] += FiFj
         A[(i+P_S)*d:(i+P_S+1)*d, (j)*d:(j+1)*d] += - FiFj
         A[(i)*d:(i+1)*d, (j+P_S)*d:(j+P_S+1)*d] += - FiFj
         A[(i+P_S)*d:(i+P_S+1)*d, (j+P_S)*d:(j+P_S+1)*d] += FiFj
+
+# print raw condition number 
+print("Computing k(A)...")
+
+kA = np.linalg.cond(A)
+
+# condition number preconditioned by diagonal 
+print("Computing k(JA)...")
+
+Jacobi = 1 / np.diag(A)
+kJ = np.linalg.cond(Jacobi * A)
+
+# condition number with nystrom sketch 
+print("Computing k(NA)...")
+
+U, S = nystrom_sketch(A, rank=10)
+rho = 1
+kN = np.linalg.cond((((S[-1] + rho) * (U / (S + rho)) @ U.T) + (np.eye(U.shape[0]) - U @ U.T)) @ A)
+
+
+print(f"Condition number of A: {kA}")
+print(f"Condition number of jacobi precond A: {kJ}")
+print(f"Condition number of nys precond A: {kN}")
